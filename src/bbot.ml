754b2e0a324ccdfe7de827cd7e24a457a87a29c8
Fabien Boucher <fboucher@redhat.com>
2020-12-07 18:10:19 +0000

    Clean the API module

--- src/bbot.ml
+++ src/bbot.ml
@@ -2,55 +2,37 @@ open Core
 open Async
 
 module Api = struct
-  (* let query_uri = Uri.of_string (String.concat [ "http://ip.jsontest.com/" ]) *)
+  let query_uri url = Uri.of_string url
 
-  let query_uri =
-    Uri.of_string
-      (String.concat
-         [ "https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m" ])
-
-  (* let decode json =
-     printf "decoding";
-     match Yojson.Safe.from_string json with
-     | `Assoc [ kv ] -> Some (Yojson.Safe.to_string (snd kv))
-     | _ -> None *)
-  let decode json =
-    printf "decoding";
-    (* manage the exception  *)
-    Binance_j.klines_of_string json
-
-  let get ~interrupt =
+  let get url decoder ~interrupt =
     try_with (fun () ->
-        Cohttp_async.Client.get ~interrupt query_uri >>= fun (_, body) ->
-        Cohttp_async.Body.to_string body >>| fun str -> decode str)
+        Cohttp_async.Client.get ~interrupt (query_uri url) >>= fun (_, body) ->
+        Cohttp_async.Body.to_string body >>| decoder)
     >>| function
     | Ok result -> Ok result
     | Error err -> Error ("Unexpected failure: " ^ Exn.to_string err)
 
-  let get_with_timeout ~timeout =
+  let get_with_timeout url decoder ~timeout =
     let interrupt = Ivar.create () in
     choose
       [
-        choice (after timeout) (fun () ->
-            Ivar.fill interrupt ();
-            Error "Timed out");
         choice
-          (get ~interrupt:(Ivar.read interrupt))
-          (fun result ->
-            let result' =
-              match result with
-              (* | Ok (Some ip) -> *)
-              | Ok klines ->
-                  printf "Done: %s" (Binance_j.string_of_klines klines);
-                  Ok "ip"
-              | Error err ->
-                  printf "Error: %s" err;
-                  Error "Unexpected failure"
-            in
-            result');
+          (after (Time.Span.of_sec timeout))
+          (fun () ->
+            Ivar.fill interrupt ();
+            Error
+              ("Timed out (" ^ string_of_float timeout ^ "s) calling: " ^ url));
+        choice (get url decoder ~interrupt:(Ivar.read interrupt)) (fun id -> id);
       ]
 end
 
 let () =
-  Api.get_with_timeout ~timeout:(Time.Span.of_sec 2.) |> ignore;
+  let url =
+    "https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m"
+  in
+  Api.get_with_timeout url Binance_j.klines_of_string ~timeout:5.0
+  >>| (function
+        | Ok jd -> jd |> Binance_j.string_of_klines |> printf "%s"
+        | Error err -> printf "%s" err)
+  |> ignore;
   never_returns (Scheduler.go ())
