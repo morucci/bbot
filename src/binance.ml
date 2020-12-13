98d89b0b0ed0b40fdfd3842323052479e3aa828d
Fabien Boucher <fboucher@redhat.com>
2020-12-13 17:16:46 +0000

    Remove last candle that is the live one

--- src/binance.ml
+++ src/binance.ml
@@ -14,7 +14,7 @@ type kline = {
 
 type klines_analysed = {
   klines : kline list;
-  macd_12_26_9 : Ta.Indicators.macd;
+  macd : Ta.Indicators.macd;
   macd_diff : float list;
 }
 
@@ -33,13 +33,21 @@ let to_kline_record (kline : Binance_t.kline) : kline =
       }
 
 let run_ta_analysys (klines : kline list) : klines_analysed =
-  let closed_prices = klines |> List.map ~f:(fun kr -> kr.c_p) in
-  let macd_12_26_9 = closed_prices |> Ta.Indicators.macd_12_26_9 in
-  let macd_diff =
-    List.map2_exn macd_12_26_9.macd_line macd_12_26_9.signal_line
-      ~f:(fun ml sl -> ml -. sl)
-  in
-  { klines; macd_12_26_9; macd_diff }
+  let klines = klines |> List.drop_last in
+  match klines with
+  | None ->
+      {
+        klines = [];
+        macd = { macd_line = []; signal_line = [] };
+        macd_diff = [];
+      }
+  | Some klines ->
+      let closed_prices = klines |> List.map ~f:(fun kr -> kr.c_p) in
+      let macd = closed_prices |> Ta.Indicators.macd_12_26_9 in
+      let macd_diff =
+        List.map2_exn macd.macd_line macd.signal_line ~f:(fun ml sl -> ml -. sl)
+      in
+      { klines; macd; macd_diff }
 
 let float_to_string f = sprintf "%.2f" f
 
@@ -53,6 +61,6 @@ let klines_analysed_to_string pair period klines_analysed depth : string =
        ~init:(pair ^ "/" ^ period ^ ":")
        ~f:(fun i init e ->
          init ^ " T-"
-         ^ (depth - i - 1 |> Int.to_string)
+         ^ (depth - i |> Int.to_string)
          ^ ":" ^ " P:" ^ (e.c_p |> float_to_string) ^ " MACD/DIFF: "
          ^ (List.nth_exn last_macd_diff i |> float_to_string))
