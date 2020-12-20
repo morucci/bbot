0b9fc44b7ba5d9142512754de90b621f6f7c8d1b
Fabien Boucher <fboucher@redhat.com>
2020-12-20 08:04:31 +0000

    Generalize in report

--- src/bbot.ml
+++ src/bbot.ml
@@ -2,7 +2,7 @@ open Core
 open Base
 open Async
 
-let klines_req_to_analysed_str (pair : string) (interval : string)
+let klines_req_to_report_entry (pair : string) (period : Report.period)
     (res : (Binance_t.kline list, string) Result.t) =
   res
   |> Result.bind ~f:(fun klines ->
@@ -10,23 +10,23 @@ let klines_req_to_analysed_str (pair : string) (interval : string)
            ( klines
            |> List.map ~f:(fun kline -> kline |> Binance.to_kline_record)
            |> Binance.run_ta_analysys
-           |> Binance.klines_analysed_to_string pair interval 4 ))
+           |> Report.klines_analysed_to_report_entry pair period 4 ))
   |> return
 
-let get_klines_to_analysed_str (pair : string) (interval : string) =
+let get_klines_to_report_entry (pair : string) (period : Report.period) =
   let url =
     "https://api.binance.com/api/v3/klines?symbol=" ^ pair ^ "&interval="
-    ^ interval
+    ^ (period |> Report.period_to_string)
   in
   Api.get_with_timeout url Binance_j.klines_of_string ~timeout:10.0
-  >>= fun res -> res |> klines_req_to_analysed_str pair interval
+  >>= fun res -> res |> klines_req_to_report_entry pair period
 
 let get_pair_to_analysed_str (pair : string) =
-  let d1 = get_klines_to_analysed_str pair "1d" in
-  let d2 = get_klines_to_analysed_str pair "1h" in
+  let d1 = get_klines_to_report_entry pair Report.ONE_H in
+  let d2 = get_klines_to_report_entry pair Report.ONE_D in
   Deferred.all [ d1; d2 ]
   >>| List.map ~f:(function
-        | Ok str -> sprintf "%s" str
+        | Ok report_entry -> report_entry |> Report.report_entry_to_string
         | Error err -> sprintf "Error: %s" err)
   >>| String.concat ~sep:"\n"
 
