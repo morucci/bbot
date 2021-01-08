d430015b89b1c464096d1e46ded46dcca23f5b6b
Fabien Boucher <fboucher@redhat.com>
2021-01-08 18:58:48 +0000

    Code churn 2

--- src/bbot.ml
+++ src/bbot.ml
@@ -2,45 +2,40 @@ open Core
 open Base
 open Async
 
-let klines_req_to_report_entry (url : string) (pair : string)
-    (period : Report_t.period) (res : (Binance_t.kline list, string) Result.t) :
-    Report_t.report_entry =
+type klines_fetcher_t =
+  [ `ONE_D | `ONE_H ] -> (Ta.Kline.t list, string) Result.t Deferred.t
+
+let get_klines_to_report_entry (pair : string) (period : Report_t.period)
+    (fetcher : (Ta.Kline.t list, string) Result.t Deferred.t)
+    (tracker_url : string) : Report_t.report_entry Deferred.t =
+  fetcher >>= fun res ->
   res
   |> Result.bind ~f:(fun klines ->
          Ok
-           ( klines
-           |> List.map ~f:(fun kline ->
-                  kline |> Binance.KFetcher.to_kline_record)
-           |> Ta.KAnalyser.run_ta_analysys
-           |> Report.Generator.klines_a_to_report_entry url pair period 4 ))
+           ( klines |> Ta.KAnalyser.run_ta_analysys
+           |> Report.Generator.klines_a_to_report_entry tracker_url pair period
+                4 ))
   |> Result.map_error ~f:(fun err ->
-         Report.Generator.make_err_report_entry url pair period 4 err)
-  |> function
-  | Ok report_entry -> report_entry
-  | Error report_entry -> report_entry
-
-let get_klines_to_report_entry (pair : string) (period : Report_t.period) :
-    Report_t.report_entry Deferred.t =
-  let period_str = period |> Report.Generator.Stringify.period_to_string in
-  Api.get_with_timeout
-    (Binance.KFetcher.get_pair_url pair period_str)
-    Binance_j.klines_of_string ~timeout:10.0
-  >>= fun res ->
-  res
-  |> klines_req_to_report_entry
-       (Binance.KFetcher.get_tracker_pair_url pair)
-       pair period
+         Report.Generator.make_err_report_entry tracker_url pair period 4 err)
+  |> (function
+       | Ok report_entry -> report_entry | Error report_entry -> report_entry)
   |> return
 
-let get_pair_to_report_entries (pair : string) :
+let get_pair_to_report_entries (fetcher : klines_fetcher_t) (pair : string) :
     Report_t.report_entry list Deferred.t =
-  let d1 = get_klines_to_report_entry pair `ONE_H in
-  let d2 = get_klines_to_report_entry pair `ONE_D in
+  let tracker_url = Binance.KFetcher.get_tracker_pair_url pair in
+  let d1 =
+    get_klines_to_report_entry pair `ONE_H (fetcher `ONE_H) tracker_url
+  in
+  let d2 =
+    get_klines_to_report_entry pair `ONE_D (fetcher `ONE_D) tracker_url
+  in
   Deferred.all [ d1; d2 ]
 
 let get_pairs_to_json_report filename =
   Binance.KFetcher.pair_list |> List.rev
-  |> List.map ~f:get_pair_to_report_entries
+  |> List.map ~f:(fun pair ->
+         get_pair_to_report_entries (Binance.KFetcher.get_klines pair) pair)
   |> Deferred.all
   >>= fun res ->
   res |> List.concat |> Report.Generator.make_report
--- src/binance.ml
+++ src/binance.ml
@@ -1,4 +1,5 @@
-open Core
+open Base
+open Async
 
 module KFetcher = struct
   let pair_list =
@@ -32,7 +33,7 @@ module KFetcher = struct
   let get_tracker_pair_url (pair : string) : string =
     "https://www.binance.com/fr/trade/" ^ pair ^ "?layout=pro"
 
-  let to_kline_record (kline : Binance_t.kline) : Ta.Kline.t =
+  let to_takline (kline : Binance_t.kline) : Ta.Kline.t =
     kline |> function
     | o_t, o_p, h_p, l_p, c_p, vol, c_t, _, trades, _, _, _ ->
         Ta.Kline.
@@ -46,4 +47,18 @@ module KFetcher = struct
             c_t;
             trades;
           }
+
+  let klines_req_to_taklines (res : (Binance_t.kline list, string) Result.t) :
+      (Ta.Kline.t list, string) Result.t =
+    res
+    |> Result.bind ~f:(fun klines ->
+           Ok (klines |> List.map ~f:(fun kline -> kline |> to_takline)))
+
+  let get_klines (pair : string) (period : Report_t.period) :
+      (Ta.Kline.t list, string) Result.t Deferred.t =
+    let period_str = period |> Report.Generator.Stringify.period_to_string in
+    Api.get_with_timeout
+      (get_pair_url pair period_str)
+      Binance_j.klines_of_string ~timeout:10.0
+    >>= fun res -> res |> klines_req_to_taklines |> return
 end
